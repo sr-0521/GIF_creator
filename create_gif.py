39f1f0e3f83f698befe35705f0c51eb1667efe88
sr-0521 <159591069+sr-0521@users.noreply.github.com>
2025-12-26 22:32:36 -0500

    First commit

---
+++ create_gif.py
@@ -0,0 +1,13 @@
+# Create a GIF with Python 🎞️
+# Sonny Li
+
+import imageio.v3 as iio
+
+filenames = ['team-pic1.png', 'team-pic2.png']
+images = [ ]
+
+for filename in filenames:
+  images.append(iio.imread(filename))
+
+iio.imwrite('team.gif', images, duration = 500, loop = 0)
+
